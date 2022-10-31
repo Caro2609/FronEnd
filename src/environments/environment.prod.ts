5c5d49c7cdea48a4a5f6d5f9c64e7c7b9fe42ad6
Caro <ccaro2609@gmail.com>
2022-10-31 14:50:10 -0300

    "modificaciones primer commit"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  firebase: {
+    projectId: 'frtedcmr',
+    appId: '1:112724398700:web:259ccf698015b3e674fe49',
+    storageBucket: 'frtedcmr.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAkDz6IlLqAlqzZkFDgKwBcSW8c3qt7u7U',
+    authDomain: 'frtedcmr.firebaseapp.com',
+    messagingSenderId: '112724398700',
+  },
+  production: true,
+  URL: 'https://cmrbke.herokuapp.com/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  firebase: {
+    projectId: 'frtedcmr',
+    appId: '1:112724398700:web:259ccf698015b3e674fe49',
+    storageBucket: 'frtedcmr.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyAkDz6IlLqAlqzZkFDgKwBcSW8c3qt7u7U',
+    authDomain: 'frtedcmr.firebaseapp.com',
+    messagingSenderId: '112724398700',
+  },
+  production: false,
+  URL: 'http://localhost:8080/'
 };
 
 /*
